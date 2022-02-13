698c0267f4d35a488b411bf3b3792960873f07b9
Diron Kelly <diron0007@gmail.com>
2022-02-13 14:53:47 -0500

    adding Challenge 15 code and resources

---
+++ MechaCarChallenge.R
@@ -0,0 +1,51 @@
+### CHALLENGE 15: DELIVERABLE 1
+
+# 3. Use the library() function to load the dplyr package
+library(dplyr)
+
+#4. Import and read in the MechaCar_mpg.csv file as a dataframe.
+library(tidyverse)
+mecha_mpg_df <- read.csv(file='./Resources/MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) 
+
+#5. Perform linear regression using the lm() function
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg_df)
+
+#6. Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_mpg_df)) 
+
+
+
+
+### CHALLENGE 15: DELIVERABLE 2
+
+#2. Import and read in the Suspension_Coil.csv file as a table
+suspension_coil_df <- read.csv(file='./Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
+
+#3. Create a total_summary dataframe using the summarize() function to get the mean, median, variance, and standard deviation of the suspension coil's PSI column.
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI),
+                                          Median = median(PSI),
+                                          Var = var(PSI),
+                                          Std_Dev = sd(PSI))
+total_summary
+#4. Create a lot_summary dataframe using the group_by() and the summarize() functions to group each manufacturing lot.                                                                
+lot_summary <- suspension_coil_df  %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),
+                                                                         Median = median(PSI),
+                                                                         Var = var(PSI),
+                                                                         Std_Dev = sd(PSI))                                      
+lot_summary
+
+### CHALLENGE 15: DELIVERABLE 3
+
+#1. use t.test() to determine if the PSI across ALL lots is statistically different from the pop. mean of 1,500 PSI.
+t.test(suspension_coil_df$PSI,mu=1500)
+
+
+#2. Use t.test() function 3 more times with subset() to determine if PSI for each manufacturing lot is statistically different from the pop. mean of 1,500 PSI
+lot1 <- subset(suspension_coil_df, Manufacturing_Lot=="Lot1")
+lot2 <- subset(suspension_coil_df, Manufacturing_Lot=="Lot2")
+lot3 <- subset(suspension_coil_df, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
+
